82b7e82a202fae1dfad26e29b8d059b1fe5f1322
lavertyaf <lavertyaileen@gmail.com>
2018-03-14 12:08:49 +0000

    Pub initial tests done

--- pub.rb
+++ pub.rb
@@ -1,13 +1,21 @@
 class Pub
 
-attr_reader
+attr_reader :name, :balance, :menu
 
   def initialize(new_name, amount_in_till, collection_of_drinks)
 
     @name = new_name
     @balance = amount_in_till
     @menu = collection_of_drinks
+  end
 
-  end  
+  def is_the_till_empty?
+    return @balance == 0
+    true
+  end
+
+  def there_are_drinks
+    @menu.length
+  end
 
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -16,6 +16,18 @@ class PubTest < MiniTest::Test
     drinks_menu = [drink1, drink2, drink3, drink4]
 
     @pub = Pub.new("The Ship", 150, drinks_menu)
-  end 
+  end
+
+  def test_pub_name
+    assert_equal("The Ship", @pub.name)
+  end
+
+  def test_till_has_money
+    assert_equal(false, @pub.is_the_till_empty?)
+  end
+
+  def test_pub_has_drinks
+    assert_equal(4, @pub.there_are_drinks)
+  end
 
 end
